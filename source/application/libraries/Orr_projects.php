a004b95ca563983df2c2b802c195ce75b72c8728
suchart bu <suchartbu@gmail.com>
2018-04-14 17:10:33 +0700

    Initial Orr-apps source

---
+++ source/application/libraries/Orr_projects.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Orr_projects extends functions from Grocey CRUD
+ * API and Functions list
+ * 
+ *
+ * @package Orr-projects
+ * @author Suchart Bunhachirat <suchartbu@gmail.com>
+ * 
+ */
+
+/**
+ * 
+ */
+class Orr_projects extends Grocery_CRUD {
+
+    /**
+     * default value of input field
+     * @var array 
+     */
+    protected $default_as = [];
+
+    /**
+     *
+     * Constructor
+     *
+     * @access  public
+     */
+    public function __construct() {
+        parent::__construct();
+        try {
+            $this->field_type('id', 'readonly')->field_type('sec_time', 'readonly')->field_type('sec_script', 'readonly');
+        } catch (Exception $e) {
+            show_error($e->getMessage() . ' --- ' . $e->getTraceAsString());
+        }
+    }
+
+    /**
+     *
+     * Default value of the field.
+     * field type can use : string , readonly
+     * @param $field_name
+     * @param $default_as
+     * @return void
+     */
+    public function default_as($field_name, $default_as = null) {
+
+        if (is_array($field_name)) {
+            foreach ($field_name as $field => $default_as) {
+                $this->default_as[$field] = $default_as;
+            }
+        } elseif ($default_as !== null) {
+            $this->default_as[$field_name] = $default_as;
+        }
+        return $this;
+    }
+
+    /**
+     * 
+     * @param object $field_info
+     * @param $value
+     * @return void
+     */
+    private function get_value($field_info, $value) {
+        if (isset($this->default_as[$field_info->name]) && empty($value)) {
+            $value = $this->default_as[$field_info->name];
+        }
+        return $value;
+    }
+
+    /**
+     * 
+     * Override method for default value.
+     */
+    protected function get_string_input($field_info, $value) {
+        return parent::get_string_input($field_info, $this->get_value($field_info, $value));
+    }
+
+    /**
+     * 
+     * Override method for default value.
+     */
+    protected function get_readonly_input($field_info, $value) {
+        return parent::get_readonly_input($field_info, $this->get_value($field_info, $value));
+    }
+
+    /**
+     * 
+     * Override method for default value.
+     */
+    protected function get_dropdown_input($field_info, $value) {
+        return parent::get_dropdown_input($field_info, $this->get_value($field_info, $value));
+    }
+
+    /**
+     * 
+     * Override method for default value.
+     */
+    protected function get_integer_input($field_info, $value) {
+        return parent::get_integer_input($field_info, $this->get_value($field_info, $value));
+    }
+
+}
