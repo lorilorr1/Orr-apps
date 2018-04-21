21af880995d2d7881fd25ad39fce5dc15b61f06d
suchartbu <suchartbu@gmail.com>
2018-04-21 19:57:21 +0700

    Add orr-projects is templete.

---
+++ orr_projects_before/orr-projects/idea/Or!Project.mm
@@ -0,0 +1,94 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1212415578329" ID="Freemind_Link_38604312" MODIFIED="1281667158066" TEXT="Or!Project">
+<font NAME="Lomaputta" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1212415622144" ID="_" MODIFIED="1256095425964" POSITION="right" TEXT="&#xe42;&#xe1b;&#xe23;&#xe41;&#xe01;&#xe23;&#xe21;&#xe17;&#xe35;&#xe48;&#xe1e;&#xe31;&#xe12;&#xe19;&#xe32;&#xe14;&#xe49;&#xe27;&#xe22; Or!Lib &#xe2a;&#xe32;&#xe21;&#xe32;&#xe23;&#xe16;&#xe43;&#xe0a;&#xe49;&#xe07;&#xe32;&#xe19;&#xe44;&#xe14;&#xe49;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Lomaputta" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223209902305" ID="Freemind_Link_1816414949" MODIFIED="1256095425957" TEXT="&#xe04;&#xe07;&#xe42;&#xe04;&#xe23;&#xe07;&#xe2a;&#xe23;&#xe49;&#xe32;&#xe07;&#xe04;&#xe33;&#xe2a;&#xe31;&#xe48;&#xe07;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+<node COLOR="#990000" CREATED="1226298888561" ID="ID_385067238" MODIFIED="1256095425950" TEXT="&#xe41;&#xe01;&#xe49;&#xe44;&#xe02;&#xe42;&#xe2d;&#xe19;&#xe22;&#xe49;&#xe32;&#xe22; Or!Config">
+<font NAME="Lomaputta" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1223209920631" ID="Freemind_Link_330790487" MODIFIED="1256095425943" TEXT="&#xe40;&#xe1e;&#xe34;&#xe48;&#xe21;&#xe04;&#xe33;&#xe2a;&#xe31;&#xe48;&#xe07;&#xe43;&#xe2b;&#xe21;&#xe48;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1212415683371" ID="Freemind_Link_1249350543" MODIFIED="1256095425907" POSITION="left" TEXT="&#xe17;&#xe38;&#xe01;&#xe42;&#xe04;&#xe23;&#xe07;&#xe07;&#xe32;&#xe19;&#xe43;&#xe0a;&#xe49; Code &#xe23;&#xe48;&#xe27;&#xe21;&#xe01;&#xe31;&#xe19;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Lomaputta" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223209935719" ID="Freemind_Link_1402738907" MODIFIED="1256095425897" TEXT="&#xe08;&#xe31;&#xe14;&#xe42;&#xe04;&#xe23;&#xe07;&#xe2a;&#xe23;&#xe49;&#xe32;&#xe07;&#xe01;&#xe32;&#xe23;&#xe40;&#xe01;&#xe47;&#xe1a;&#xe44;&#xe1f;&#xe25;&#xe4c;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224991544911" ID="Freemind_Link_391968572" MODIFIED="1256095425887" TEXT="&#xe23;&#xe32;&#xe22;&#xe25;&#xe30;&#xe40;&#xe2d;&#xe35;&#xe22;&#xe14;&#xe40;&#xe01;&#xe35;&#xe48;&#xe22;&#xe27;&#xe01;&#xe31;&#xe1a;&#xe42;&#xe04;&#xe23;&#xe07;&#xe2a;&#xe23;&#xe49;&#xe32;&#xe07;&#xe44;&#xe1f;&#xe25;&#xe4c;">
+<font NAME="Lomaputta" SIZE="14"/>
+<node COLOR="#111111" CREATED="1226205500110" ID="ID_619004783" MODIFIED="1256095425877" TEXT="&#xe43;&#xe0a;&#xe49; skins &#xe23;&#xe48;&#xe27;&#xe21;&#xe01;&#xe31;&#xe19;">
+<font NAME="Lomaputta" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1226205524992" ID="ID_310664603" MODIFIED="1256095425867" TEXT="&#xe43;&#xe0a;&#xe48; LIb &#xe23;&#xe48;&#xe27;&#xe21;&#xe01;&#xe31;&#xe19;">
+<font NAME="Lomaputta" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1224991665438" ID="Freemind_Link_1149506438" MODIFIED="1256095425859" TEXT="&#xe15;&#xe31;&#xe19;&#xe41;&#xe1a;&#xe1a;&#xe42;&#xe04;&#xe23;&#xe07;&#xe2a;&#xe23;&#xe49;&#xe32;&#xe07;&#xe44;&#xe1f;&#xe25;&#xe4c;">
+<font NAME="Lomaputta" SIZE="14"/>
+<node COLOR="#111111" CREATED="1224994799330" ID="Freemind_Link_875356631" MODIFIED="1281666710626" TEXT="default.html">
+<font NAME="Lomaputta" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1223209961916" ID="Freemind_Link_38578398" MODIFIED="1256095425835" TEXT="&#xe08;&#xe31;&#xe14;&#xe42;&#xe04;&#xe23;&#xe07;&#xe2a;&#xe23;&#xe49;&#xe32;&#xe07;&#xe10;&#xe32;&#xe19;&#xe02;&#xe49;&#xe2d;&#xe21;&#xe39;&#xe25;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1212415798262" ID="Freemind_Link_216371982" MODIFIED="1281667159980" POSITION="right" TEXT="&#xe23;&#xe30;&#xe1a;&#xe1a;&#xe2a;&#xe48;&#xe27;&#xe19;&#xe01;&#xe25;&#xe32;&#xe07;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Lomaputta" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1214642512133" ID="Freemind_Link_1513778002" MODIFIED="1256095425822" TEXT="&#xe40;&#xe21;&#xe19;&#xe39;&#xe23;&#xe30;&#xe1a;&#xe1a;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1214642556285" ID="Freemind_Link_1605622572" MODIFIED="1256095425815" TEXT="&#xe23;&#xe30;&#xe1a;&#xe1a;&#xe41;&#xe01;&#xe49;&#xe44;&#xe02;&#xe02;&#xe49;&#xe2d;&#xe21;&#xe39;&#xe25;&#xe2a;&#xe48;&#xe27;&#xe19;&#xe15;&#xe31;&#xe27;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1281666516337" ID="ID_222442328" MODIFIED="1281666623988" TEXT="&#xe23;&#xe30;&#xe1a;&#xe1a;&#xe44;&#xe1f;&#xe25;&#xe4c;&#xe2a;&#xe16;&#xe32;&#xe19;&#xe30;&#xe01;&#xe32;&#xe23;&#xe43;&#xe0a;&#xe49;&#xe07;&#xe32;&#xe19;&#xe2a;&#xe48;&#xe27;&#xe19;&#xe15;&#xe31;&#xe27;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1281667164892" ID="ID_635136580" MODIFIED="1281667188725" TEXT="&#xe23;&#xe30;&#xe1a;&#xe1a;&#xe23;&#xe31;&#xe1a;&#xe2a;&#xe48;&#xe07;&#xe02;&#xe49;&#xe2d;&#xe04;&#xe27;&#xe32;&#xe21;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1212415853629" ID="Freemind_Link_270629197" MODIFIED="1256095425809" POSITION="left" TEXT="Web2.0">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Lomaputta" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223775050883" ID="Freemind_Link_387518518" MODIFIED="1256095425803" TEXT="Dojo">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+<icon BUILTIN="attach"/>
+<node COLOR="#990000" CREATED="1223775074078" ID="Freemind_Link_843561628" MODIFIED="1281666652739" TEXT="form">
+<font NAME="Lomaputta" SIZE="14"/>
+<node COLOR="#111111" CREATED="1281666654624" ID="ID_515262694" MODIFIED="1281666675707" TEXT="&#xe15;&#xe23;&#xe27;&#xe08;&#xe2a;&#xe2d;&#xe1a;&#xe02;&#xe49;&#xe2d;&#xe21;&#xe39;&#xe25;&#xe01;&#xe48;&#xe2d;&#xe19;&#xe2a;&#xe48;&#xe07;"/>
+</node>
+<node COLOR="#990000" CREATED="1223775082749" ID="Freemind_Link_1019292779" MODIFIED="1256095425790" TEXT="controls">
+<font NAME="Lomaputta" SIZE="14"/>
+<node COLOR="#111111" CREATED="1281667738660" ID="ID_633044077" MODIFIED="1281667755974" TEXT="&#xe15;&#xe23;&#xe27;&#xe08;&#xe2a;&#xe2d;&#xe1a;&#xe23;&#xe39;&#xe1b;&#xe41;&#xe1a;&#xe1a;&#xe02;&#xe49;&#xe2d;&#xe21;&#xe39;&#xe25;&#xe44;&#xe14;&#xe49;"/>
+</node>
+<node COLOR="#990000" CREATED="1223775109224" ID="Freemind_Link_1864278120" MODIFIED="1256095425784" TEXT="tooltip">
+<font NAME="Lomaputta" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1223775056692" ID="Freemind_Link_90621036" MODIFIED="1281667768334" TEXT="Ext">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Lomaputta" SIZE="16"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</map>
